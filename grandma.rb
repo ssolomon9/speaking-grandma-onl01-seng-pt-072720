389bc5697ef5b254a62db777fecdb444c29e511d
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-06 22:55:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phase)
   elsif phrase.upcase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
   elsif phase == "Hi Nana, how are you?"
-    return 
+    return "HUH?! SPEAK UP, SONNY!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
