1d6eca2b3cad4ede39179cb1ef44c492386a9f67
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-06 23:37:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,7 @@ def speak_to_grandma(phase)
   elsif phase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
   else 
+  # else state doesn't need a argument 
     return "NO, NOT SINCE 1938!"
   end
 end
