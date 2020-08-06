b315d77d7b97f7e52b30e2894e91a83e6d28c201
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-06 22:54:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
+  elsif phase == "Hi Nana, how are you?"
+    return 
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
